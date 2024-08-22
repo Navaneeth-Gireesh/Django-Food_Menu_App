78eef0cbe09fe52745505f2e137c713b00bfffef
Navaneeth <navaneeth2002hitech@gmail.com>
2024-08-22 22:05:08 +0530

    Changed Function based view of list view and details view to class based view

--- food_menu/food/urls.py
+++ food_menu/food/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('details/<int:item_id>', views.details, name = 'details'),
+    path('', views.IndexClassView.as_view(), name = 'index'),
+    path('details/<int:pk>', views.DetailsClassView.as_view(), name = 'details'),
+    
     path('add_item', views.add_item, name = 'add_item'),
     path('edit_details/<int:item_id>', views.edit_details, name = 'edit_details'),
     path('delete_item,<int:item_id>', views.delete_item, name = 'delete_item'),
--- food_menu/food/views.py
+++ food_menu/food/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from . models import Item
 from . forms import AddItemForm
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 
-def index(request):
-    food_items = Item.objects.all()
-    return render(request, 'index.html', {'food_items': food_items})
 
-def details(request,item_id):
-    food_details = get_object_or_404(Item, id=item_id)
-    return render(request, 'details.html', {'food_details': food_details})
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'index.html'
+    context_object_name = 'food_items'
+
+
+class DetailsClassView(DetailView):
+    model = Item
+    template_name = 'details.html'
+
 
 def add_item(request):
     if request.method == 'POST':
